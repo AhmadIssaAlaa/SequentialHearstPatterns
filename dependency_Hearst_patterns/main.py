f0edb5794a9c65d0827310fe55e0c331822f62e8
AhmadIssaAlaa <ahmad_alaa_eddein@hotmail.com>
2019-02-06 17:22:20 +0200

    Update main.py

--- dependency_Hearst_patterns/main.py
+++ dependency_Hearst_patterns/main.py
@@ -9,6 +9,7 @@ def main():
     -neg_samples_file: a file path for samples of negative sentences (same number of semantically positive sentences may be selected randomly)
     -sem_pos_processed_file: a file path for samples of negative sentences after processing (result of java preprocessing step)
     -output_files: a list of output files, each one corresponds for a specific DHP
+    -dataset_path: the path for the dataset file
     """
     sem_pos_file = r"..\labeled_corpus\Music_Sem_Pos.txt"
     sem_pos_processed_file = r"..\processed_corpus\Music_Sem_Pos_processed.txt"
@@ -20,7 +21,7 @@ def main():
                     r"..\matching_DHP_subcorpora\matching_and_other.txt",
                     r"..\matching_DHP_subcorpora\matching_especially.txt",
                     r"..\matching_DHP_subcorpora\matching_such_NP_as.txt"]
-
+    dataset_path = r"..\datasets\Music.txt"
     patterns = ["NP such as NP", "NP including NP", "NP is a NP", "NP and other NP", "NP especially NP",
                 "such NP as NP"]
     f0 = open(output_files[0], "w")
@@ -30,8 +31,8 @@ def main():
     f4 = open(output_files[4], "w")
     f5 = open(output_files[5], "w")
 
-    matching_DHP_and_write_into_files(sem_pos_file, sem_pos_processed_file, patterns, f0, f1, f2, f3, f4, f5)
-    matching_DHP_and_write_into_files(neg_samples_file, neg_samples_processed_file, patterns, f0, f1, f2, f3, f4, f5)
+    matching_DHP_and_write_into_files(sem_pos_file, sem_pos_processed_file, patterns, f0, f1, f2, f3, f4, f5, "positive", dataset_path)
+    matching_DHP_and_write_into_files(neg_samples_file, neg_samples_processed_file, patterns, f0, f1, f2, f3, f4, f5, "negative", dataset_path)
     f0.close()
     f1.close()
     f2.close()
@@ -39,7 +40,7 @@ def main():
     f4.close()
     f5.close()
 
-def matching_DHP_and_write_into_files(file, processed_file, patterns, f0, f1, f2, f3, f4, f5):
+def matching_DHP_and_write_into_files(file, processed_file, patterns, f0, f1, f2, f3, f4, f5, label, dataset_path):
     sentences = open(file).readlines()
     i = 0
     for parsed_sentence in cf.get_sentences(processed_file):
@@ -48,36 +49,20 @@ def matching_DHP_and_write_into_files(file, processed_file, patterns, f0, f1, f2
         i += 1
         res = DHP_matching(parsed_sentence, sentence)
         if res[0]:
+            predicted, predicted_by = cf.check_extracted_couples(res[1], dataset_path)
             if res[2][0] == patterns[0]:
-                f0.write("<s>\n")
-                f0.write(res[3] + "\n")
-                f0.write(str(res[1]) + "\n")
-                f0.write("</s>\n")
+                cf.write_sentence_matching_result_into_file(f0, res[3], res[1], label, predicted, predicted_by)
             elif res[2][0] == patterns[1]:
-                f1.write("<s>\n")
-                f1.write(res[3] + "\n")
-                f1.write(str(res[1]) + "\n")
-                f1.write("</s>\n")
+                cf.write_sentence_matching_result_into_file(f1, res[3], res[1], label, predicted, predicted_by)
             elif res[2][0] == patterns[2]:
-                f2.write("<s>\n")
-                f2.write(res[3] + "\n")
-                f2.write(str(res[1]) + "\n")
-                f2.write("</s>\n")
+                cf.write_sentence_matching_result_into_file(f2, res[3], res[1], label, predicted, predicted_by)
             elif res[2][0] == patterns[3]:
-                f3.write("<s>\n")
-                f3.write(res[3] + "\n")
-                f3.write(str(res[1]) + "\n")
-                f3.write("</s>\n")
+                cf.write_sentence_matching_result_into_file(f3, res[3], res[1], label, predicted, predicted_by)
             elif res[2][0] == patterns[4]:
-                f4.write("<s>\n")
-                f4.write(res[3] + "\n")
-                f4.write(str(res[1]) + "\n")
-                f4.write("</s>\n")
+                cf.write_sentence_matching_result_into_file(f4, res[3], res[1], label, predicted, predicted_by)
             elif res[2][0] == patterns[5]:
-                f5.write("<s>\n")
-                f5.write(res[3] + "\n")
-                f5.write(str(res[1]) + "\n")
-                f5.write("</s>\n")
+                cf.write_sentence_matching_result_into_file(f5, res[3], res[1], label, predicted, predicted_by)
+
 
 
 if __name__ == '__main__':
